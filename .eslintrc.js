fc625cf0d0fc8ada7d5f002160288a4156280f6f
Kennys Ng <kennysng@hotmail.com.hk>
2018-07-01 23:08:34 +0800

    eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    node: true,
+    jasmine: true
+  },
+  plugins: [
+    'babel',
+    'standard'
+  ],
+  extends: [
+    'standard'
+  ]
+}
