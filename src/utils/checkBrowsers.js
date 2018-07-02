7a4d5bba3477db204d03d86b4da10ed1d67a3340
Kennys Ng <kennysng@hotmail.com.hk>
2018-07-02 19:03:15 +0800

    auto add broswerslist to package.json

--- src/utils/checkBrowsers.js
+++ src/utils/checkBrowsers.js
@@ -1,7 +1,6 @@
 const browserslist = require('browserslist')
 const chalk = require('chalk')
 const fs = require('fs')
-const inquirer = require('inquirer')
 const os = require('os')
 const pkgUp = require('pkg-up')
 
@@ -25,50 +24,34 @@ module.exports = defaultBrowsers => function checkBrowsers (dir, retry = true) {
     )
   }
 
-  const question = {
-    type: 'confirm',
-    name: 'shouldSetBrowsers',
-    message:
-      chalk.yellow("We're unable to detect target browsers.") +
-      `\n\nWould you like to add the defaults to your ${chalk.bold(
-        'package.json'
-      )}?`,
-    default: true
-  }
-  return inquirer.prompt(question).then(answer => {
-    if (answer.shouldSetBrowsers) {
-      return (
-        pkgUp(dir)
-          .then(filePath => {
-            if (filePath == null) {
-              return Promise.reject(new Error('package.json file not found'))
-            }
-            const pkg = JSON.parse(fs.readFileSync(filePath))
-            pkg['browserslist'] = defaultBrowsers
-            fs.writeFileSync(filePath, JSON.stringify(pkg, null, 2) + os.EOL)
+  return (
+    pkgUp(dir)
+      .then(filePath => {
+        if (filePath == null) {
+          return Promise.reject(new Error('package.json file not found'))
+        }
+        const pkg = JSON.parse(fs.readFileSync(filePath))
+        pkg['browserslist'] = defaultBrowsers
+        fs.writeFileSync(filePath, JSON.stringify(pkg, null, 2) + os.EOL)
 
-            browserslist.clearCaches()
-            console.log()
-            console.log(chalk.green('Set target browsers:'))
-            console.log()
-            console.log(
-              `\t${chalk.bold('Production')}: ${chalk.cyan(
-                defaultBrowsers.production.join(', ')
-              )}`
-            )
-            console.log(
-              `\t${chalk.bold('Development')}: ${chalk.cyan(
-                defaultBrowsers.development.join(', ')
-              )}`
-            )
-            console.log()
-          })
-          // Swallow any error
-          .catch(() => {})
-          .then(() => checkBrowsers(dir, false))
-      )
-    } else {
-      return checkBrowsers(dir, false)
-    }
-  })
+        browserslist.clearCaches()
+        console.log()
+        console.log(chalk.green('Set target browsers:'))
+        console.log()
+        console.log(
+          `\t${chalk.bold('Production')}: ${chalk.cyan(
+            defaultBrowsers.production.join(', ')
+          )}`
+        )
+        console.log(
+          `\t${chalk.bold('Development')}: ${chalk.cyan(
+            defaultBrowsers.development.join(', ')
+          )}`
+        )
+        console.log()
+      })
+      // Swallow any error
+      .catch(() => {})
+      .then(() => checkBrowsers(dir, false))
+  )
 }
