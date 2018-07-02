8669cbe9e19848e993ea37c38a60dc9506893b9a
Kennys Ng <kennysng@hotmail.com.hk>
2018-07-02 18:48:01 +0800

    update env.js

--- src/utils/env.js
+++ src/utils/env.js
@@ -3,10 +3,14 @@ const _ = require('lodash')
 const path = require('path')
 
 module.exports = jsonPath => {
-  let env = {}
   if (jsonPath) {
-    jsonPath = path.resolve(process.cwd(), jsonPath)
-    try { env = require(jsonPath) } catch (e) { debug(`WARNING path '${jsonPath}' not exist`) }
+    let env = {}
+    if (jsonPath) {
+      jsonPath = path.resolve(process.cwd(), jsonPath)
+      try { env = require(jsonPath) } catch (e) { debug(`WARNING path '${jsonPath}' not exist`) }
+    }
+    return (process.env = _.merge(process.env, env))
+  } else {
+    return process.env
   }
-  return (process.env = _.merge(process.env, env))
 }
