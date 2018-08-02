1afff6ed6b4c8351c818e241a30f5ef4368fc219
Kennys Ng <kennysng@hotmail.com.hk>
2018-08-02 12:24:32 +0800

    fix: export sequence

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = {
   build: require('./src/build'),
-  config: {
-    build: require('./config/build.config'),
-    start: require('./start/build.config')
-  },
   start: require('./src/start'),
   utils: require('./src/utils')
+}
+module.exports.config = {
+  build: require('./config/build.config'),
+  start: require('./start/build.config')
 }
