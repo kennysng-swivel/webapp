16c544d83ee96e632886546cd06f89ceccb465b6
Kennys Ng <kennysng@hotmail.com.hk>
2018-08-08 09:28:30 +0800

    fix: noncachedRequire's path issue

--- src/utils/require.js
+++ src/utils/require.js
@@ -1,4 +1,11 @@
+const caller = require('caller-callsite')
+const path = require('path')
+
 module.exports = id => {
-  delete require.cache[require.resolve(id)]
+  if (!path.isAbsolute(id)) {
+    const callerPath = caller().getFileName()
+    id = path.resolve(path.dirname(callerPath), id)
+  }
+  delete require.cache[id]
   return require(id)
 }
